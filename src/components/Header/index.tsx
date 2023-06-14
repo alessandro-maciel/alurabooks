c7488bf773ffd341c517156bee59217f62146ef6
Alessandro Maciel <alessandromaciel237@gmail.com>
2023-06-13 21:17:55 -0300

    feat: componente header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import {ReactComponent as Logo} from '../../assets/logo.svg';
+import {ReactComponent as UserIcon} from '../../assets/user.svg';
+import Categorias from "./components/Categorias";
+import {Container, HeaderContainer, HeaderLogo, Login} from './styles';
+
+export default function Header(){
+    return (
+        <Container>
+            <HeaderContainer>
+                <HeaderLogo>
+                    <Logo width={40} height={40}/>
+                    <h1>AluraBooks</h1>
+                </HeaderLogo>
+                <Categorias />
+            </HeaderContainer>
+            <Login>
+                <UserIcon width={32} height={32}/>
+                <p>Login</p>
+            </Login>
+        </Container>
+    );
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,45 @@
+import { styled } from "styled-components"
+
+export const Container = styled.nav`
+    background: #FFFFFF;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 18px 80px;
+    align-items: center;
+`
+
+export const HeaderContainer = styled.nav`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`
+
+export const HeaderLogo = styled.div`
+    display: flex;
+    flex-direction: row;
+    h1 {
+        font-size: 30px;
+        line-height: 30px;
+        color: #000000;
+        font-family: 'Josefin Sans', sans-serif;
+        font-weight: 700;
+        display: flex;
+        align-items: center;
+        margin-top: 10px;
+        margin-left: 11px;
+    }
+`
+
+export const Login = styled.div`
+    display: flex;
+    flex-direction: row;
+    p {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 30px;
+        color: #000000;
+        margin-left: 8px;
+    }
+`
