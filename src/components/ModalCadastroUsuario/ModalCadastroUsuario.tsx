892e7765c72ec7626edecd2749c69220037ec0e6
Alessandro Maciel <alessandromaciel237@gmail.com>
2023-06-25 10:50:49 -0300

    feat: finalizando tela de cadastro de usuário

--- src/components/ModalCadastroUsuario/ModalCadastroUsuario.tsx
+++ src/components/ModalCadastroUsuario/ModalCadastroUsuario.tsx
@@ -1,12 +1,89 @@
-import { AbModal } from "alurabooks-ds-alessandro-maciel";
+import { AbBotao, AbCampoTexto, AbModal } from "alurabooks-ds-alessandro-maciel";
+
+import imagem from './assets/login.png';
+import { useState } from "react";
+import { Container, Form, Footer, ComplementoCep } from "./styles";
 
 export default function ModalCadastroUsuario(){
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [endereco, setEndereco] = useState('');
+    const [complemento, setComplemento] = useState('');
+    const [cep, setCep] = useState('');
+    const [senha, setSenha] = useState('');
+    const [confirmaSenha, setConfirmaSenha] = useState('');
+
     return (
         <AbModal 
             titulo="CADASTRO"
             visivel={true}
         >
-            <h1>Teste</h1>
+            <Container>
+                <figure>
+                    <img src={imagem} alt="Monitor com uma fechadura e uma pessoa com uma chave logo ao lado" />
+                </figure>
+                
+                <Form>
+                    <AbCampoTexto
+                        value={nome}
+                        label="Nome"
+                        placeholder="Seu nome completo"
+                        type="text"
+                        onChange={setNome}
+                    />
+                    <AbCampoTexto
+                        value={email}
+                        label="Email"
+                        placeholder="seuemail@maneiro.com.br"
+                        type="text"
+                        onChange={setEmail}
+                    />
+                    <AbCampoTexto
+                        value={endereco}
+                        label="Endereco"
+                        placeholder="Sua rua e número"
+                        type="text"
+                        onChange={setEndereco}
+                    />
+                    <ComplementoCep>
+                        <AbCampoTexto
+                            value={complemento}
+                            label="Complemento"
+                            placeholder="Apto/casa, bloco, referência"
+                            type="text"
+                            onChange={setComplemento}
+                        />
+                        <AbCampoTexto
+                            value={cep}
+                            label="Cep"
+                            placeholder="Apto/casa e bloco"
+                            type="text"
+                            onChange={setCep}
+                        />
+                    </ComplementoCep>
+                    <AbCampoTexto
+                            value={senha}
+                            label="Senha"
+                            placeholder="************"
+                            type="password"
+                            onChange={setSenha}
+                        />
+                    <AbCampoTexto
+                        value={confirmaSenha}
+                        label="Confirmar senha"
+                        placeholder="************"
+                        type="password"
+                        onChange={setConfirmaSenha}
+                    />
+                    <Footer>
+                        <AbBotao 
+                            texto="Cadastrar"
+                            tipo="primario"
+                            onPress={() => {}}
+                        />
+                    </Footer>
+                </Form>
+            </Container>
         </AbModal>
     );
 }
---
+++ src/components/ModalCadastroUsuario/styles.ts
@@ -0,0 +1,23 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    gap: 16px;
+    align-items: center;
+`
+
+export const ComplementoCep = styled.div`
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+`
+
+export const Form = styled.form`
+    width: 500px;
+    max-width: 100%;
+`
+
+export const Footer = styled.div`
+    display:flex;
+    margin-top: 46px;
+    justify-content: center;
+`
